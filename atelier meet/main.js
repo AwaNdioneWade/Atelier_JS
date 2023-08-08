25a1be76a14c6bd0f2e9d737684cdc4b58de44cc
Awa Ndione Wade <ndionewadeawa0804@gmail.com>
2023-08-08 21:10:39 +0000

    ajout atelier meet

---
+++ atelier meet/main.js
@@ -0,0 +1,31 @@
+const divBalises = document.getElementById("balises")
+const inputbalise = document.getElementById("input")
+const choosebalise = document.getElementById("choose")
+const enonce = document.getElementById("enonce")
+
+
+function insererbalise(balises, textAInserer) {
+    let baliseHTML =  document.createElement('p');
+    baliseHTML.innertext = (`Choisissez une des balises HTML pour encradrer`);
+    enonce.append(baliseHTML.innertext);
+    
+    choosebalise.addEventListener('click', function(){
+
+        if (inputbalise.value == ""){
+        alert('Veillez d\'abord entrer le nom de la balise');
+        }
+    
+        else{
+            balises = inputbalise.value
+            const balise = document.createElement(balises);
+            const textbalise = document.createTextNode(textAInserer);
+        
+            balise.append(textbalise);        
+            divBalises.append(balise);
+            
+            inputbalise.value = "";
+        }
+    }) 
+}
+console.log(divBalises)
+insererbalise(balises, "Mon texte")
