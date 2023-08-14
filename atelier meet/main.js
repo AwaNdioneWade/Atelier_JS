7fbbebb8120240e317d34fb02985904567a88dc5
Awa Ndione Wade <ndionewadeawa0804@gmail.com>
2023-08-14 21:31:40 +0000

    stylisation

--- atelier meet/main.js
+++ atelier meet/main.js
@@ -3,11 +3,12 @@ const inputText = document.getElementById("textAInserer")
 const inputbalise = document.getElementById("input")
 const choosebalise = document.getElementById("choose")
 const enonce = document.getElementById("enonce")
+const texte = document.getElementById("text")
 
 function insererbalise(balises, textAInserer) {
-    let baliseHTML =  document.createElement('p');
-    baliseHTML.innertext = (`Entrez votre texte et choisissez une balise HTML pour l'entourer`);
-    enonce.append(baliseHTML.innertext);
+    let baliseHTML = document.createElement('h4');
+    baliseHTML.innerHTML = (`Entrez votre texte et choisissez une balise HTML pour l'entourer`);
+    enonce.append(baliseHTML);
     
     choosebalise.addEventListener('click', function(){
         balises = inputbalise.value
@@ -17,14 +18,14 @@ function insererbalise(balises, textAInserer) {
             const tabBalisesHTML = ["h1", "h2", "h3", "h4", "h5", "h6", "p", "span", "em", "b"]
             let tableauEnMajuscules = tabBalisesHTML.map(element => element.toUpperCase());
 
-            if (tabBalisesHTML.indexOf(balises) === -1 & tableauEnMajuscules.indexOf(balises) === -1) {
+            if (tabBalisesHTML.indexOf(balises) === -1 && tableauEnMajuscules.indexOf(balises) === -1) {
                 alert(`Vous devrez mettre une balise HTML qui entoure un texte par exemple la balise p ou h1`);                
             } else {
                     const balise = document.createElement(balises);
                     textAInserer = inputText.value
                     balise.innertext = textAInserer
                     balise.append(textAInserer);        
-                    divBalises.append(balise);
+                    text.append(balise);
             }
             inputbalise.value = "";
             inputText.value = "";
