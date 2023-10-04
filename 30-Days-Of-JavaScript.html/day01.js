e630e1ff5331e5828084c7845edd3a3b995f7818
Awa Ndione Wade <ndionewadeawa0804@gmail.com>
2023-10-04 23:37:21 +0000

    30 day of JS

---
+++ 30-Days-Of-JavaScript.html/day01.js
@@ -0,0 +1,29 @@
+//  Affiche "Hello, World!"
+console.log('Hello, World!')
+
+console.log('Welcome to 30DaysOfJavaScript')
+
+// Type variable
+
+let firstName = 'Asabeneh'    // String
+let lastName = 'Yetayeh'      
+let country = 'Finland'       
+let city = 'Helsinki'         
+let age = 100                 // Int
+let isMarried = true          // Booleen
+
+// Declaration variables number
+
+const gravity = 9.81
+const boilingPoint = 100
+const PI = 3.14             
+
+// Déclaration variable sur une seule ligne 
+
+let name = 'Asabeneh', job = 'teacher', live = 'Finland'
+
+
+  // Afficher variable
+console.log(firstName, lastName, country, city, age, isMarried) 
+console.log(gravity, boilingPoint, PI) 
+console.log(name, job, live)
