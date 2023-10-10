4515ff0d715cda8617ad8e7e53449044afdf290c
Awa Ndione Wade <ndionewadeawa0804@gmail.com>
2023-10-10 09:30:55 +0000

    day06

---
+++ 30-Days-Of-JavaScript.html/day06/main.js
@@ -0,0 +1,43 @@
+const numbers = [1, 2, 3, 4, 5]
+let sum = 0
+for(let i = 0; i < numbers.length; i++){
+  sum  = sum + numbers[i]  
+
+}
+
+console.log(sum)  // 15
+
+
+
+numbers = [1, 2, 3, 4, 5]
+
+for (const num of numbers) {
+  console.log(num)
+}
+
+// 1 2 3 4 5
+
+for (const num of numbers) {
+  console.log(num * num)
+}
+
+// 1 4 9 16 25
+
+const webTechs = [
+  'HTML',
+  'CSS',
+  'JavaScript',
+  'React',
+  'Redux',
+  'Node',
+  'MongoDB'
+]
+
+for (const tech of webTechs) {
+  console.log(tech.toUpperCase())
+}
+
+
+for (const tech of webTechs) {
+  console.log(tech[0]) // H C J R N M
+}
