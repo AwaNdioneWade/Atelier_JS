61523ca1dea5abc81cf16e66e81d504dd07083e8
Awa Ndione Wade <ndionewadeawa0804@gmail.com>
2023-10-18 09:57:30 +0000

    fetch

--- atelier9/fetch.js
+++ atelier9/fetch.js
@@ -1,5 +1,3 @@
-
-
 // async function fetchUsers () {
 //     const r = await fetch('https://jsonplaceholder.typicode.com/todos/')
 //     if(r.ok === true){
@@ -11,17 +9,32 @@
 // fetchUsers().then(users => console.log(users))
 
 
-const a = new AbortController()
-Promise.race([
-    fetch('https://jsonplaceholder.typicode.com/todos/?_limit=5&_delay=2000',{
-        signal: a.signal
-    }),
-    fetch('https://jsonplaceholder.typicode.com/todos/?_limit=3', {
-        signal: a.signal
-        })
+// const a = new AbortController()
+// Promise.race([
+//     fetch('https://jsonplaceholder.typicode.com/todos/?_limit=5&_delay=2000',{
+//         signal: a.signal
+//     }),
+//     fetch('https://jsonplaceholder.typicode.com/todos/?_limit=3', {
+//         signal: a.signal
+//         })
     
     
-]).then(r => r.json()).then(body => {
-    a.abort()
-    console.log(body);
+// ]).then(r => r.json()).then(body => {
+//     a.abort()
+//     console.log(body);
+// }) 
+
+fetch('https://jsonplaceholder.typicode.com/todos/')
+.then(r => r.json()).then(users => {
+users.forEach(user => {
+    let utilisateurs = document.getElementById('users')
+    let ligne = utilisateurs.insertRow(-1)
+    let id = ligne.insertCell(0)
+    let title = ligne.insertCell(1)
+    let completed = ligne.insertCell(2)
+    id.textContent = `${user['id']}`
+    title.textContent = `${user['title']}`
+    completed.textContent = `${user['completed']}`
+    });
+    console.log(users);
 })
